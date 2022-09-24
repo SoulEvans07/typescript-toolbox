3d85889363af0d03f59edd796d20d19a2d9dcc21
Adam Szi <szi.adam@simonyi.bme.hu>
2022-09-24 16:50:16 +0200

    polyfills: object fromFunctions

--- ts-utils/src/polyfills/object/index.ts
+++ ts-utils/src/polyfills/object/index.ts
@@ -1,6 +1,24 @@
+if (!Object.keysTyped) {
+  Object.keysTyped = function <T extends UnknownObject>(obj: T): Array<keyof T> {
+    return Object.keys(obj) as Array<keyof T>;
+  };
+}
+
+if (!Object.entriesTyped) {
+  Object.entriesTyped = function <T extends UnknownObject>(obj: T): Array<[keyof T, T[keyof T]]> {
+    return Object.entries(obj) as Array<[keyof T, T[keyof T]]>;
+  };
+}
+
 if (!Object.prototype.isProperty) {
   Object.prototype.isProperty = function <T extends UnknownObject>(this: T, key: keyof T): key is PropertyKeys<T> {
     return typeof this[key] !== 'function';
+  };
+}
+
+if (!Object.prototype.isFunction) {
+  Object.prototype.isFunction = function <T extends UnknownObject>(this: T, key: keyof T): key is FunctionKeys<T> {
+    return typeof this[key] === 'function';
   };
 }
 
@@ -16,14 +34,44 @@ if (!Object.prototype.fromProperties) {
   };
 }
 
-if (!Object.keysTyped) {
-  Object.keysTyped = function <T extends UnknownObject>(obj: T): Array<keyof T> {
-    return Object.keys(obj) as Array<keyof T>;
+if (!Object.prototype.fromFunctions) {
+  Object.prototype.fromFunctions = function <T extends UnknownObject>(this: T): Functions<T> {
+    const props = {} as Functions<T>;
+
+    const classType = Object.getPrototypeOf(this);
+    const keys = Object.getOwnPropertyNames(classType).filter(k => k !== 'constructor') as Array<keyof T>;
+    keys.forEach(key => {
+      if (this.isFunction(key)) props[key] = this[key];
+    });
+
+    return props;
   };
 }
 
-if (!Object.entriesTyped) {
-  Object.entriesTyped = function <T extends UnknownObject>(obj: T): Array<[keyof T, T[keyof T]]> {
-    return Object.entries(obj) as Array<[keyof T, T[keyof T]]>;
+// eslint-disable-next-line @typescript-eslint/ban-types
+function getAllKeys<T extends Object>(toCheck: T): Array<keyof T> {
+  const props: Array<keyof T> = [];
+
+  let obj = toCheck;
+  do {
+    const keys = Object.getOwnPropertyNames(obj) as Array<keyof T>;
+    // const filtered = keys.filter(k => Object.hasOwn(toCheck, k));
+    props.push(...keys);
+  } while ((obj = Object.getPrototypeOf(obj)) && obj.constructor.name !== 'Object');
+
+  return props.sort().filter(k => k !== 'constructor');
+}
+
+if (!Object.prototype.fromAllFunctions) {
+  Object.prototype.fromAllFunctions = function <T extends UnknownObject>(this: T): Functions<T> {
+    const props = {} as Functions<T>;
+
+    const classType = Object.getPrototypeOf(this);
+    const keys = getAllKeys(classType) as Array<keyof T>;
+    keys.forEach(key => {
+      if (this.isFunction(key)) props[key] = this[key];
+    });
+
+    return props;
   };
 }
--- ts-utils/src/polyfills/object/object.d.ts
+++ ts-utils/src/polyfills/object/object.d.ts
@@ -1,6 +1,9 @@
 declare interface Object {
   isProperty<T extends UnknownObject>(this: T, key: keyof T): key is PropertyKeys<T>;
   fromProperties<T extends UnknownObject>(this: T): Properties<T>;
+  isFunction<T extends UnknownObject>(this: T, key: keyof T): key is FunctionKeys<T>;
+  fromFunctions<T extends UnknownObject>(this: T): Functions<T>;
+  fromAllFunctions<T extends UnknownObject>(this: T): Functions<T>;
 }
 
 declare interface ObjectConstructor {
@@ -8,7 +11,16 @@ declare interface ObjectConstructor {
   entriesTyped<T extends UnknownObject>(obj: T): Array<[keyof T, T[keyof T]]>;
 }
 
-declare type UnknownObject = Record<string, unknown>;
-// eslint-disable-next-line @typescript-eslint/ban-types
-declare type PropertyKeys<T extends UnknownObject> = { [K in keyof T]: T[K] extends Function ? never : K }[keyof T];
 declare type Properties<T extends UnknownObject> = Pick<T, PropertyKeys<T>>;
+declare type Functions<T extends UnknownObject> = Pick<T, FunctionKeys<T>>;
+declare type PropertyKeys<T extends UnknownObject> = {
+  [K in keyof T]: T[K] extends UnknownFunction ? never : K;
+}[keyof T];
+declare type FunctionKeys<T extends UnknownObject> = {
+  [K in keyof T]: T[K] extends UnknownFunction ? K : never;
+}[keyof T];
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+declare type UnknownObject = Object;
+// eslint-disable-next-line @typescript-eslint/ban-types
+declare type UnknownFunction = Function;
